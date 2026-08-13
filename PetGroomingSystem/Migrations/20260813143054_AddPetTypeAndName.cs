a3748e0d93bc0994aee7fbaa857792b62a898cff
jaclynnn29 <angelinajaclyn00@gmail.com>
2026-08-13 22:52:12 +0800

    added smtg

---
+++ PetGroomingSystem/Migrations/20260813143054_AddPetTypeAndName.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetGroomingSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPetTypeAndName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PetName",
+                table: "Appointments",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PetType",
+                table: "Appointments",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PetName",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "PetType",
+                table: "Appointments");
+        }
+    }
+}
