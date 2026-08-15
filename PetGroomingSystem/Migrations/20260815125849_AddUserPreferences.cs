a230f9f78f6a0e6d18e49dcfcb9a24caa533844f
jaclynnn29 <angelinajaclyn00@gmail.com>
2026-08-15 21:10:48 +0800

    fixed dark mode bcuz when change to dark mode the font colour cannot see

---
+++ PetGroomingSystem/Migrations/20260815125849_AddUserPreferences.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetGroomingSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserPreferences : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NavOrder",
+                table: "Members",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PreferredTheme",
+                table: "Members",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NavOrder",
+                table: "Members");
+
+            migrationBuilder.DropColumn(
+                name: "PreferredTheme",
+                table: "Members");
+        }
+    }
+}
